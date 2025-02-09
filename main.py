99c16e1e9a63a8247a30104df2bccc28cd2c9c9c
Pavel N <npa2003@gmail.com>
2025-02-09 21:58:16 +0300

    add scale image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-#from pygame.examples.scrap_clipboard import screen
 
 pygame.init()
 SCREEN_WIDTH = 800
@@ -12,17 +11,34 @@ icon = pygame.image.load('image/main_icon.png')
 pygame.display.set_icon(icon)
 
 target_image = pygame.image.load('image/target_icon.png')
+
+target_width = target_image.get_width() # получаем размер изображения
+target_height = target_image.get_height()
+print(target_width, target_height)
+
+target_width, target_width = target_image.get_size() # получаем размер изображения альтернативно
+print(target_width, target_height)
+
 target_width = 80
 target_height = 80
+target_image = pygame.transform.scale(target_image,(target_width, target_height))
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+print(color)
 
 
 running = True
 while running:
-    pass
+    screen.fill(color) # заполняем поле цветом
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
 
 
 
